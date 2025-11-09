96c75f54573fc9073c3cbded0eea8a3090939697
Avery <raifujo.tachi@gmail.com>
2025-11-09 17:56:08 +0100

    As always problems with github pages

--- dist/bundle.js
+++ dist/bundle.js
@@ -3275,7 +3275,7 @@ async function getShaders() {
   return object;
 }
 async function getModels() {
-  const model_source = "../models";
+  const model_source = "./models";
   const models_names = [
     "lander",
     "rover",
